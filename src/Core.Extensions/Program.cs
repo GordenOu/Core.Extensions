b4b00aae06d99dfef504065b2534167c1e55bdd2
OU Qihao <qihao.ou@connect.polyu.hk>
2016-06-28 04:32:07 +0800

    Add project files.

---
+++ src/Core.Extensions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Extensions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Yo~");
+        }
+    }
+}
