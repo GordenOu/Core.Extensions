91c469a4f280b146f345ed7edefc22fa68bf6633
Gorden Ou <qihao.ou@connect.ust.hk>
2016-08-06 01:52:01 +0800

    Add Core.Reflection.

---
+++ src/Core.Reflection/TypeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using Core.Diagnostics;
+
+namespace Core.Reflection
+{
+    /// <summary>
+    /// Provides methods that retrieve information about types at run time.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Gets a default value of the specified type.
+        /// </summary>
+        /// <param name="type">The specified type.</param>
+        /// <returns>The default value of the specified type.</returns>
+        public static object GetDefaultValue(this Type type)
+        {
+            Requires.NotNull(type, nameof(type));
+
+            return type.GetTypeInfo().IsValueType ? Activator.CreateInstance(type) : null;
+        }
+    }
+}
---
+++ test/Core.Reflection.Tests/TypeExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Reflection.Tests
+{
+    [TestClass]
+    public class TypeExtensionsTests
+    {
+        [TestMethod]
+        public void GetDefaultValueOfClassAndStruct()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => ((Type)null).GetDefaultValue());
+
+            Assert.IsNull(typeof(TypeExtensionsTests).GetDefaultValue());
+            Assert.AreEqual(default(DateTime), typeof(DateTime).GetDefaultValue());
+        }
+    }
+}
