fb575a2e59a274366b82b807f5c993b49dae2e27
GD <qihao.ou@connect.ust.hk>
2021-03-22 23:34:07 +0800

    Core.Buffers

---
+++ Core.Extensions.Tests/Core.Buffers.Tests/PrimitivesTests.cs
@@ -0,0 +1,290 @@
+using System.Buffers;
+using System.Buffers.Binary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Buffers.Tests
+{
+    [TestClass]
+    public class PrimitivesTests
+    {
+        [TestMethod]
+        public void ReadDoubleValues()
+        {
+            var buffer = new byte[16];
+            var sequence = new ReadOnlySequence<byte>(buffer);
+
+            BinaryPrimitives.WriteDoubleBigEndian(buffer, 1);
+
+            bool result = Primitives.TryReadDoubleBigEndian(sequence, out double value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadDoubleBigEndian(sequence.Slice(0, 8), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadDoubleBigEndian(sequence.Slice(0, 7), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+
+            BinaryPrimitives.WriteDoubleLittleEndian(buffer, 1);
+
+            result = Primitives.TryReadDoubleLittleEndian(sequence, out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadDoubleLittleEndian(sequence.Slice(0, 8), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadDoubleLittleEndian(sequence.Slice(0, 7), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+        }
+
+        [TestMethod]
+        public void ReadInt16Values()
+        {
+            var buffer = new byte[4];
+            var sequence = new ReadOnlySequence<byte>(buffer);
+
+            BinaryPrimitives.WriteInt16BigEndian(buffer, 1);
+
+            bool result = Primitives.TryReadInt16BigEndian(sequence, out short value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt16BigEndian(sequence.Slice(0, 2), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt16BigEndian(sequence.Slice(0, 1), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+
+            BinaryPrimitives.WriteInt16LittleEndian(buffer, 1);
+
+            result = Primitives.TryReadInt16LittleEndian(sequence, out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt16LittleEndian(sequence.Slice(0, 2), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt16LittleEndian(sequence.Slice(0, 1), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+        }
+
+        [TestMethod]
+        public void ReadInt32Values()
+        {
+            var buffer = new byte[8];
+            var sequence = new ReadOnlySequence<byte>(buffer);
+
+            BinaryPrimitives.WriteInt32BigEndian(buffer, 1);
+
+            bool result = Primitives.TryReadInt32BigEndian(sequence, out int value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt32BigEndian(sequence.Slice(0, 4), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt32BigEndian(sequence.Slice(0, 3), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+
+            BinaryPrimitives.WriteInt32LittleEndian(buffer, 1);
+
+            result = Primitives.TryReadInt32LittleEndian(sequence, out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt32LittleEndian(sequence.Slice(0, 4), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt32LittleEndian(sequence.Slice(0, 3), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+        }
+
+        [TestMethod]
+        public void ReadInt64Values()
+        {
+            var buffer = new byte[16];
+            var sequence = new ReadOnlySequence<byte>(buffer);
+
+            BinaryPrimitives.WriteInt64BigEndian(buffer, 1);
+
+            bool result = Primitives.TryReadInt64BigEndian(sequence, out long value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt64BigEndian(sequence.Slice(0, 8), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt64BigEndian(sequence.Slice(0, 7), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+
+            BinaryPrimitives.WriteInt64LittleEndian(buffer, 1);
+
+            result = Primitives.TryReadInt64LittleEndian(sequence, out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt64LittleEndian(sequence.Slice(0, 8), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadInt64LittleEndian(sequence.Slice(0, 7), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+        }
+
+        [TestMethod]
+        public void ReadSingleValues()
+        {
+            var buffer = new byte[8];
+            var sequence = new ReadOnlySequence<byte>(buffer);
+
+            BinaryPrimitives.WriteSingleBigEndian(buffer, 1);
+
+            bool result = Primitives.TryReadSingleBigEndian(sequence, out float value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadSingleBigEndian(sequence.Slice(0, 4), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadSingleBigEndian(sequence.Slice(0, 3), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+
+            BinaryPrimitives.WriteSingleLittleEndian(buffer, 1);
+
+            result = Primitives.TryReadSingleLittleEndian(sequence, out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadSingleLittleEndian(sequence.Slice(0, 4), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadSingleLittleEndian(sequence.Slice(0, 3), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+        }
+
+        [TestMethod]
+        public void ReadUInt16Values()
+        {
+            var buffer = new byte[4];
+            var sequence = new ReadOnlySequence<byte>(buffer);
+
+            BinaryPrimitives.WriteUInt16BigEndian(buffer, 1);
+
+            bool result = Primitives.TryReadUInt16BigEndian(sequence, out ushort value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadUInt16BigEndian(sequence.Slice(0, 2), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadUInt16BigEndian(sequence.Slice(0, 1), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+
+            BinaryPrimitives.WriteUInt16LittleEndian(buffer, 1);
+
+            result = Primitives.TryReadUInt16LittleEndian(sequence, out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadUInt16LittleEndian(sequence.Slice(0, 2), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(1, value);
+
+            result = Primitives.TryReadUInt16LittleEndian(sequence.Slice(0, 1), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+        }
+
+        [TestMethod]
+        public void ReadUInt32Values()
+        {
+            var buffer = new byte[8];
+            var sequence = new ReadOnlySequence<byte>(buffer);
+
+            BinaryPrimitives.WriteUInt32BigEndian(buffer, 1);
+
+            bool result = Primitives.TryReadUInt32BigEndian(sequence, out uint value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<uint>(1, value);
+
+            result = Primitives.TryReadUInt32BigEndian(sequence.Slice(0, 4), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<uint>(1, value);
+
+            result = Primitives.TryReadUInt32BigEndian(sequence.Slice(0, 3), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+
+            BinaryPrimitives.WriteUInt32LittleEndian(buffer, 1);
+
+            result = Primitives.TryReadUInt32LittleEndian(sequence, out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<uint>(1, value);
+
+            result = Primitives.TryReadUInt32LittleEndian(sequence.Slice(0, 4), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<uint>(1, value);
+
+            result = Primitives.TryReadUInt32LittleEndian(sequence.Slice(0, 3), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+        }
+
+        [TestMethod]
+        public void ReadUInt64Values()
+        {
+            var buffer = new byte[16];
+            var sequence = new ReadOnlySequence<byte>(buffer);
+
+            BinaryPrimitives.WriteUInt64BigEndian(buffer, 1);
+
+            bool result = Primitives.TryReadUInt64BigEndian(sequence, out ulong value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<ulong>(1, value);
+
+            result = Primitives.TryReadUInt64BigEndian(sequence.Slice(0, 8), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<ulong>(1, value);
+
+            result = Primitives.TryReadUInt64BigEndian(sequence.Slice(0, 7), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+
+            BinaryPrimitives.WriteUInt64LittleEndian(buffer, 1);
+
+            result = Primitives.TryReadUInt64LittleEndian(sequence, out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<ulong>(1, value);
+
+            result = Primitives.TryReadUInt64LittleEndian(sequence.Slice(0, 8), out value);
+            Assert.AreEqual(true, result);
+            Assert.AreEqual<ulong>(1, value);
+
+            result = Primitives.TryReadUInt64LittleEndian(sequence.Slice(0, 7), out value);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(default, value);
+        }
+    }
+}
---
+++ Core.Extensions/Core.Buffers/Primitives.cs
@@ -0,0 +1,346 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Core.Buffers
+{
+    /// <summary>
+    /// Reads bytes as primitives with specific endianness.
+    /// </summary>
+    public unsafe static class Primitives
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool TryRead16BitBigEndian<T>(ReadOnlySequence<byte> source, out T value)
+            where T : unmanaged
+        {
+            Debug.Assert(sizeof(T) == 2);
+
+            var reader = new SequenceReader<byte>(source);
+            if (reader.TryReadBigEndian(out short shortValue))
+            {
+                T* pValue = (T*)&shortValue;
+                value = *pValue;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool TryRead16BitLittleEndian<T>(ReadOnlySequence<byte> source, out T value)
+            where T : unmanaged
+        {
+            Debug.Assert(sizeof(T) == 2);
+
+            var reader = new SequenceReader<byte>(source);
+            if (reader.TryReadLittleEndian(out short shortValue))
+            {
+                T* pValue = (T*)&shortValue;
+                value = *pValue;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool TryRead32BitBigEndian<T>(ReadOnlySequence<byte> source, out T value)
+            where T : unmanaged
+        {
+            Debug.Assert(sizeof(T) == 4);
+
+            var reader = new SequenceReader<byte>(source);
+            if (reader.TryReadBigEndian(out int intValue))
+            {
+                T* pValue = (T*)&intValue;
+                value = *pValue;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool TryRead32BitLittleEndian<T>(ReadOnlySequence<byte> source, out T value)
+            where T : unmanaged
+        {
+            Debug.Assert(sizeof(T) == 4);
+
+            var reader = new SequenceReader<byte>(source);
+            if (reader.TryReadLittleEndian(out int intValue))
+            {
+                T* pValue = (T*)&intValue;
+                value = *pValue;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool TryRead64BitBigEndian<T>(ReadOnlySequence<byte> source, out T value)
+            where T : unmanaged
+        {
+            Debug.Assert(sizeof(T) == 8);
+
+            var reader = new SequenceReader<byte>(source);
+            if (reader.TryReadBigEndian(out long longValue))
+            {
+                T* pValue = (T*)&longValue;
+                value = *pValue;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool TryRead64BitLittleEndian<T>(ReadOnlySequence<byte> source, out T value)
+            where T : unmanaged
+        {
+            Debug.Assert(sizeof(T) == 8);
+
+            var reader = new SequenceReader<byte>(source);
+            if (reader.TryReadLittleEndian(out long longValue))
+            {
+                T* pValue = (T*)&longValue;
+                value = *pValue;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Reads a System.Double from the beginning of a read-only sequence of bytes, as big endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as big endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Double; otherwise, false.</returns>
+        public static bool TryReadDoubleBigEndian(ReadOnlySequence<byte> source, out double value)
+        {
+            return TryRead64BitBigEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Double from the beginning of a read-only sequence of bytes, as little endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as little endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Double; otherwise, false.</returns>
+        public static bool TryReadDoubleLittleEndian(ReadOnlySequence<byte> source, out double value)
+        {
+            return TryRead64BitLittleEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Int16 from the beginning of a read-only sequence of bytes, as big endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as big endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Int16; otherwise, false.</returns>
+        public static bool TryReadInt16BigEndian(ReadOnlySequence<byte> source, out short value)
+        {
+            var reader = new SequenceReader<byte>(source);
+            return reader.TryReadBigEndian(out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Int16 from the beginning of a read-only sequence of bytes, as little endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as little endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Int16; otherwise, false.</returns>
+        public static bool TryReadInt16LittleEndian(ReadOnlySequence<byte> source, out short value)
+        {
+            var reader = new SequenceReader<byte>(source);
+            return reader.TryReadLittleEndian(out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Int32 from the beginning of a read-only sequence of bytes, as big endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as big endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Int32; otherwise, false.</returns>
+        public static bool TryReadInt32BigEndian(ReadOnlySequence<byte> source, out int value)
+        {
+            var reader = new SequenceReader<byte>(source);
+            return reader.TryReadBigEndian(out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Int32 from the beginning of a read-only sequence of bytes, as little endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as little endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Int32; otherwise, false.</returns>
+        public static bool TryReadInt32LittleEndian(ReadOnlySequence<byte> source, out int value)
+        {
+            var reader = new SequenceReader<byte>(source);
+            return reader.TryReadLittleEndian(out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Int64 from the beginning of a read-only sequence of bytes, as big endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as big endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Int64; otherwise, false.</returns>
+        public static bool TryReadInt64BigEndian(ReadOnlySequence<byte> source, out long value)
+        {
+            var reader = new SequenceReader<byte>(source);
+            return reader.TryReadBigEndian(out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Int64 from the beginning of a read-only sequence of bytes, as little endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as little endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Int64; otherwise, false.</returns>
+        public static bool TryReadInt64LittleEndian(ReadOnlySequence<byte> source, out long value)
+        {
+            var reader = new SequenceReader<byte>(source);
+            return reader.TryReadLittleEndian(out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Single from the beginning of a read-only sequence of bytes, as big endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as big endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Single; otherwise, false.</returns>
+        public static bool TryReadSingleBigEndian(ReadOnlySequence<byte> source, out float value)
+        {
+            return TryRead32BitBigEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.Single from the beginning of a read-only sequence of bytes, as little endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as little endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.Single; otherwise, false.</returns>
+        public static bool TryReadSingleLittleEndian(ReadOnlySequence<byte> source, out float value)
+        {
+            return TryRead32BitLittleEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.UInt16 from the beginning of a read-only sequence of bytes, as big endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as big endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.UInt16; otherwise, false.</returns>
+        public static bool TryReadUInt16BigEndian(ReadOnlySequence<byte> source, out ushort value)
+        {
+            return TryRead16BitBigEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.UInt16 from the beginning of a read-only sequence of bytes, as little endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as little endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.UInt16; otherwise, false.</returns>
+        public static bool TryReadUInt16LittleEndian(ReadOnlySequence<byte> source, out ushort value)
+        {
+            return TryRead16BitLittleEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.UInt32 from the beginning of a read-only sequence of bytes, as big endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as big endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.UInt32; otherwise, false.</returns>
+        public static bool TryReadUInt32BigEndian(ReadOnlySequence<byte> source, out uint value)
+        {
+            return TryRead32BitBigEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.UInt32 from the beginning of a read-only sequence of bytes, as little endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as little endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.UInt32; otherwise, false.</returns>
+        public static bool TryReadUInt32LittleEndian(ReadOnlySequence<byte> source, out uint value)
+        {
+            return TryRead32BitLittleEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.UInt64 from the beginning of a read-only sequence of bytes, as big endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as big endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.UInt64; otherwise, false.</returns>
+        public static bool TryReadUInt64BigEndian(ReadOnlySequence<byte> source, out ulong value)
+        {
+            return TryRead64BitBigEndian(source, out value);
+        }
+
+        /// <summary>
+        /// Reads a System.UInt64 from the beginning of a read-only sequence of bytes, as little endian.
+        /// </summary>
+        /// <param name="source">The read-only sequence of bytes to read.</param>
+        /// <param name="value">
+        /// When this method returns, contains the value read out of the read-only sequence of bytes, as little endian.
+        /// </param>
+        /// <returns>true if the span is large enough to contain a System.UInt64; otherwise, false.</returns>
+        public static bool TryReadUInt64LittleEndian(ReadOnlySequence<byte> source, out ulong value)
+        {
+            return TryRead64BitLittleEndian(source, out value);
+        }
+    }
+}
